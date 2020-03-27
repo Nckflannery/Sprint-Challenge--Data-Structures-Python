4a277eda9cf67d6ec32997e3248f5242be1e95e0
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-27 11:51:38 -0700

    Names fixed

--- names/names.py
+++ names/names.py
@@ -35,53 +35,107 @@ list. bad bad bad
 
 # Let's use an LRU cache
 
-class LRUCache:
+# class LRUCache:
 
-    def __init__(self, limit=10):
-        self.limit = limit
-        self.lis = DoublyLinkedList()
-        self.size = 0
-        self.cache = {}
+#     def __init__(self, limit=10):
+#         self.limit = limit
+#         self.lis = DoublyLinkedList()
+#         self.size = 0
+#         self.cache = {}
 
 
-    def get(self, key):
-        if key in self.cache:
-            out = self.cache[key]
-            self.lis.move_to_front(out)
-            return out.value[1]
-        else:
-            return None
+#     def get(self, key):
+#         if key in self.cache:
+#             out = self.cache[key]
+#             self.lis.move_to_front(out)
+#             return out.value[1]
+#         else:
+#             return None
 
-    def set(self, key, value):
-        if key in self.cache:
-            inp = self.cache[key]
-            inp.value = (key, value)
-            self.lis.move_to_front(inp)
-            return inp
+#     def set(self, key, value):
+#         if key in self.cache:
+#             inp = self.cache[key]
+#             inp.value = (key, value)
+#             self.lis.move_to_front(inp)
+#             return inp
         
-        if self.size is self.limit:
-            self.cache.pop(self.lis.remove_from_tail()[0])
-            self.size -=1
-        self.lis.add_to_head((key, value))
-        self.cache[key] = self.lis.head
-        self.size += 1
+#         if self.size is self.limit:
+#             self.cache.pop(self.lis.remove_from_tail()[0])
+#             self.size -=1
+#         self.lis.add_to_head((key, value))
+#         self.cache[key] = self.lis.head
+#         self.size += 1
 
-# Create instance of LRUCache with limit 10,000 (len(names))
-lru = LRUCache(10000)
+# # Create instance of LRUCache with limit 10,000 (len(names))
+# lru = LRUCache(10000)
+
+# for i in names_1:
+#     lru.set(i, i)
+
+# for i in names_2:
+#     if lru.get(i) == i:
+#         duplicates.append(i)
+
+# '''
+# LRUCache: 
+
+# 64 duplicates
+# runtime: 0.02150416374206543 seconds
+# Huge improvement
+# '''
+
+
+# Binary Search Tree
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        elif value >= self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target > self.value:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+
+tree = BinarySearchTree(names_1[0])
 
 for i in names_1:
-    lru.set(i, i)
+    tree.insert(i)
 
-for i in names_2:
-    if lru.get(i) == i:
-        duplicates.append(i)
+for j in names_2:
+    if tree.contains(j):
+        duplicates.append(j)
 
 '''
-LRUCache: 
+Binary Search Tree:
 
 64 duplicates
-runtime: 0.02150416374206543 seconds
-Huge improvement
+runtime: 0.07851338386535645 seconds
 '''
 
 end_time = time.time()
