4b483ba99bfce7a0d172604ec4136234a6a56fda
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-27 08:38:01 -0700

    ring buffer complete, no stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,26 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # Set initial spot
+        if not self.current:
+            self.current = self.storage.head
+        # If we are at capacity, replace oldest item
+        if self.storage.length == self.capacity:
+            self.current.value = item
+            self.current = self.current.next
+        # Else, add to tail
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        # Create pointer to iter through self.storage, start at oldest (head)
+        pointer = self.storage.head
+        while pointer:
+            # Append pointer value to list, and increment using .next
+            list_buffer_contents.append(pointer.value)
+            pointer = pointer.next
 
         return list_buffer_contents
 
